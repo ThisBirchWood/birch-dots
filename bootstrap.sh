e36719997a9dd162053a89528e2c99ed0437b63c
Dylan De Faoite <dylanseandefaoite@gmail.com>
2026-03-05 09:57:40 +0000

    fix(sddm): permissions and ownership of root-level SDDM confs
    SDDM configs are at the system root level and are not user owned,
    therefore they must be owned by the root and have special permissions.
    
    `/etc/sddm.conf.d` and `/etc/sddm` *should* have permissions of 644.
    Root can read/write, everyone else can read.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## SDDM Confs
 sudo mkdir -p /etc/sddm.conf.d
 sudo cp -a system/sddm.conf.d /etc/
 sudo cp -a system/sddm /etc/
+sudo chown -R root:root /etc/sddm.conf.d /etc/sddm
+sudo chmod -R 644 /etc/sddm.conf.d /etc/sddm
